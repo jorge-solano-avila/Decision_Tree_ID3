3d40cf7066c13dee8373ce2b4cc6c4bf8480b17a
jorge52an <jasolanoa@unal.edu.co>
2016-07-26 18:29:48 -0500

    chore: add data set

---
+++ src/decisionTree.js
@@ -0,0 +1,11 @@
+data =
+{
+    attributes :
+    {
+        gender : ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'male', 'female'],
+        carOwnership : [0, 1, 1, 0, 1, 0, 1, 1, 2, 2],
+        travelCost : ['cheap', 'cheap', 'cheap', 'cheap', 'standard', 'standard', 'expensive', 'expensive', 'expensive'],
+        incomeLevel : ['low', 'medium', 'medium', 'low', 'medium', 'medium', 'medium', 'high', 'medium', 'high']
+    },
+    results : ['bus', 'bus', 'train', 'bus', 'bus', 'train', 'train', 'car', 'car', 'car']
+};
