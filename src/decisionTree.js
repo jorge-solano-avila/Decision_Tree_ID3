b99d266a52ab3de5aaa876a353ca6ca4c4c9f204
jorge52an <jasolanoa@unal.edu.co>
2016-07-26 18:36:25 -0500

    chore: add division and entropy functions

--- src/decisionTree.js
+++ src/decisionTree.js
@@ -1,3 +1,6 @@
+var node = require('./tree').node;
+var tree = require('./tree').tree;
+
 data =
 {
     attributes :
@@ -9,3 +12,54 @@ data =
     },
     results : ['bus', 'bus', 'train', 'bus', 'bus', 'train', 'train', 'car', 'car', 'car']
 };
+
+var entropy = function( probabilities )
+{
+    var entropy = 0;
+
+    for( var probability in probabilities )
+        entropy += probabilities[probability] * Math.log2( probabilities[probability] );
+
+    return -entropy;
+};
+
+var division = function( results )
+{
+    broken = false;
+    result = [];
+    probabilities = {};
+
+    for( var i = 0; i < results.length; ++i )
+    {
+        for( var j = 0; j < result.length; ++j )
+            if( result[j] === results[i] )
+            {
+                broken = true;
+                break;
+            }
+        if( !broken )
+        {
+            result.push( results[i] );
+            probabilities[results[i]] = 1;
+        }
+        else
+        {
+            ++probabilities[results[i]];
+            broken = false;
+        }
+    }
+
+    for( var probability in probabilities )
+        probabilities[probability] /= results.length;
+
+    return {
+        result : result,
+        probabilities : probabilities
+    };
+};
+
+var aux = division( data.results );
+var result = aux.result;
+var probabilities = aux.probabilities;
+
+var entropy = entropy( probabilities );
